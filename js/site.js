ecc1da9d1578243223df9e2ee9079b73ce38b354
maxg599 <109822431+maxg599@users.noreply.github.com>
2022-09-29 23:51:33 -0400

    appworks

--- js/site.js
+++ js/site.js
@@ -1,5 +1,29 @@
+//GET STRING FROM THE PAGE//
+function getValue(){
+    document.getElementById("alert").classList.add("invisible");
+    let userString = document.getElementById("userString").value;
+    let revString = reverseString(userString);
+    displayString(revString);
+}
 
-//call hello world//
-function helloWorld(){
-    alert("hello World");
+
+//REVERSE THE STRING//
+function reverseString(userString){
+    let revString = [];
+
+
+
+    for (let index = userString.length -1; index >= 0; index--) {
+       revString += userString[index];        
+    }
+
+    return revString;
+}
+
+
+
+//DISPLAY THE REVERSED STRING TO THE USER//
+function displayString(reverseString){
+    document.getElementById("msg").innerHTML = `Your String Reversed is: ${reverseString}`
+    document.getElementById("alert").classList.remove("invisible");
 }
